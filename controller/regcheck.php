f9f9930488a9c934fe1dacaece03348e9a19a1a1
Abir-Al-Arafat <62530610+Abir-Al-Arafat@users.noreply.github.com>
2021-03-10 16:36:32 +0600

    Adding functionality added

---
+++ controller/regcheck.php
@@ -0,0 +1,117 @@
+<?php  
+ $message = '';  
+ $error = '';  
+ if(isset($_POST["submit"]))  
+ {  
+      if(empty($_POST["fullname"]))  
+      {  
+           $error = "Name field cannot be empty";  
+      }
+      else if(empty($_POST["email"]))  
+      {  
+           $error = "E-mail field cannot be empty";  
+      }  
+      else if(empty($_POST["phone"]))  
+      {  
+           $error = "Phone no cannot be empty";  
+      }  
+      else if(empty($_POST["dateOfBirth"]))  
+      {  
+           $error = "date of birth cannot be empty";  
+      }
+      else if(empty($_POST["username"]))  
+      {  
+           $error = "User Name cannot be empty";  
+      } 
+      else if(empty($_POST["password"]))  
+      {  
+           $error = "password cannot be empty";  
+      }
+      else if(empty($_POST["confirmpassword"]))
+      {
+          $error = "confirm password field cannot be empty";
+      }
+      else if(empty($_POST["id"]))
+      {
+          $error = "id field cannot be empty";
+      }
+      else if(empty($_POST["type"]))
+      {
+          $error = "type field cannot be empty";
+      }
+     else  
+     {
+
+     $pass = $Cpass = ""; 
+     if(isset($_POST['password'])&&isset($_POST['confirmpassword'])) // checking if password is set or not
+     {
+        $pass = $_POST['password'];
+        $Cpass = $_POST['confirmpassword'];
+        if($pass == $Cpass) //checking if passwords match or not
+        {
+          if(file_exists('../model/admin.json'))  
+          {  
+                $current_data = file_get_contents('../model/admin.json');  
+                $array_data = json_decode($current_data, true);  
+                $extra = array(  
+                     'id'               =>     $_POST["id"],  
+                     'fullname'          =>     $_POST["fullname"],  
+                     'email'     =>     $_POST["email"],  
+                     'phone'     =>     $_POST["phone"],  
+                     'dateOfBirth'     =>     $_POST["dateOfBirth"],   
+                     'username'     =>     $_POST["username"],  
+                     'password'     =>     $_POST["password"],  
+                     'regdate'     =>     $_POST["regdate"],  
+                     'type'     =>     $_POST["type"],  
+                );  
+                $array_data[] = $extra;  
+                $final_data = json_encode($array_data);  
+                if(file_put_contents('../model/admin.json', $final_data))  
+                {  
+                    $message = "File Appended Successfully in admin.json"."<br>";                  
+                }  
+          }
+          if(file_exists('../model/login.json'))
+          {
+               $current_data_1 = file_get_contents('../model/login.json');
+               $array_data_1 = json_decode($current_data_1, true); 
+               $extra_1 = array(  
+                    'id'               =>     $_POST["id"],   
+                    'username'     =>     $_POST["username"],  
+                    'password'     =>     $_POST["password"],   
+                    'type'     =>     $_POST["type"]  
+               ); 
+               $array_data_1[] = $extra_1;
+               $final_data_1 = json_encode($array_data_1);
+               if(file_put_contents('../model/login.json',$final_data_1))
+               {
+                    $message_1 = "File Appended Successfully in login.json"."<br>";
+               }
+          }  
+          else  
+          {  
+                $error = 'JSON File does not exist';  
+          }  
+        }
+        else
+        {
+          $error = "Passwords did not match";          
+        }
+    }
+            
+          
+     }  
+ }
+     if(isset($message))  
+     {  
+     echo $message;  
+     }
+     if(isset($error))  
+     {  
+          echo $error;  
+     } 
+     if(isset($message_1))
+     {
+          echo $message_1;
+     }       
+ ?> 
---
+++ view/addUser.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel='icon' href='./images/assets/favicon.png'>
+    <title>Add a new user</title>
+</head>
+<body background="../images/assets/background.jpg">
+    <?php include('./header.php'); ?>
+    <div width='100px'>
+        <form action='../controller/regcheck.php' method="POST">
+            <fieldset>
+                <legend>
+                    <b>REGISTRATION</b>
+                </legend>
+                <table align="center">
+                    <tr>
+                        <td align="right">Id:</td>
+                        <td><input type='number' name='id'/></td>
+                    </tr>
+                    <tr>
+                        <td align="right">Full Name:</td>
+                        <td><input type='text' name='fullname'/></td>
+                    </tr>
+                    <tr>
+                        <td align="right">Email:</td>
+                        <td><input type='email' name='email'/></td>
+                    </tr>
+                    <tr>
+                        <td align="right">Phone:</td>
+                        <td><input type='text' name='phone' value="+88"/></td>
+                    </tr>
+                    <tr>
+                        <td align="right">Date of Birth:</td>
+                        <td><input type='date' name='dateOfBirth'/></td>
+                    </tr>
+                    <tr>
+                        <td align="right">Username:</td>
+                        <td><input type='text' name='username'/></td>
+                    </tr>
+                    <tr>
+                        <td align="right">Password:</td>
+                        <td><input type='text' name='password'/></td>
+                    </tr>
+                    <tr>
+                        <td align="right">Confirm Password:</td>
+                        <td><input type='text' name='confirmpassword'/></td>
+                    </tr>
+                    <tr>
+                        <td align="right">Registration date:</td>
+                        <td><input type='date' name='regdate' required/></td>
+                    </tr>
+                    <tr>
+                        <td align="right">Type:</td>
+                        <td><input type='text' name='type' value = 'admin' readonly/></td>
+                    </tr>
+                    <tr>
+                        <td colspan="2"><hr></td>
+                    </tr>
+                    <tr>
+                        <td align="center" colspan="2"><input type='submit' name = 'submit' value="Confirm Registration"></td>
+                    </tr>
+                    <tr>
+                        <td align="center" colspan="2"><input type='reset' value="Reset"></td>
+                    </tr>
+                </table>
+            </fieldset>
+        </form>
+    </div>
+</body>
+</html>
